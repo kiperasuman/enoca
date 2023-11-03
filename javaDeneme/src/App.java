3ea8bf03e4949fb5fc6b7f10ad00a22172b43924
kiperasuman <asuman.kiper00@gmail.com>
2023-11-04 00:22:03 +0300

    Question 4

---
+++ javaDeneme/src/App.java
@@ -0,0 +1,21 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Aritmetik İlerleme
+        int ilk_terim = 2;
+        int fark = 2;
+
+        for (int i = 1; i <= 5; i++) {
+            if (i == 1) {
+                System.out.println("*");
+            }
+            for (int j = 0; j < ilk_terim + (i - 1) * fark; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
